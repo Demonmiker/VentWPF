acd7791a0c27900d01c69b56f710d597eb1fbb9b
demonmiker <demonmiker@ya.ru>
2021-06-23 13:20:09 +0300

    рефактор кулеров

--- VentWPF/ViewModel/Elements/Coolers/Cooler_Fr.cs
+++ VentWPF/ViewModel/Elements/Coolers/Cooler_Fr.cs
@@ -14,7 +14,7 @@ namespace VentWPF.ViewModel
             Name = "Охладитель фреоновый";
             image = "Coolers/Cooler_Fr.png";
         }
-
+        #region Данные
         [DisplayName("t наружного воздуха")]
         [Category(c1), PropertyOrder(2)]
         public float tOutside { get; set; } = 30;
@@ -26,9 +26,9 @@ namespace VentWPF.ViewModel
         [DisplayName("Влажность наружного воздуха")]
         [Category(c1), PropertyOrder(6)]
         public float humidityOutSide { get; set; } = 42;
+        #endregion
 
-        private float AB = (((float)project.With / 1000) * ((float)project.Height / 1000));
-
+        #region Информация 
         [DisplayName("Падение давления расчётное")]
         [Category(c2), PropertyOrder(1)]
         public override float PressureDrop => (70 / (4 / (((float)project.VFlow / 3600) / AB)));
@@ -41,18 +41,24 @@ namespace VentWPF.ViewModel
         [Category(c1), PropertyOrder(7)]
         public FrType Fr { get; set; }
 
-        [Browsable(false)]
-        public float pD => (float)(Math.Exp((1500.3 + 23.5 * tOutside) / (234 + tOutside)));
-
         [DisplayName("Абсолютная влажность воздуха на выходе")]
         [Category(c2), PropertyOrder(4)]
         public float humidityOut => (float)((0.6222 * (humidityOutSide / 100) * pD) / (project.PressOut - (humidityOutSide / 100) * pD / 1000));
 
-        [Browsable(false)]
-        public float pD2 => (float)(Math.Exp((1500.3 + 23.5 * tOut) / (234 + tOut)));
 
         [DisplayName("Относительная влажность воздуха на выходе")]
         [Category(c2), PropertyOrder(4)]
         public int humidityOutOtn => (int)((project.PressOut / pD2 * 1000 / (0.6222 / humidityOut * 1000 + 1)) * 100);
+        #endregion
+
+        private float AB = (((float)project.With / 1000) * ((float)project.Height / 1000));
+
+        [Browsable(false)]
+        public float pD => (float)(Math.Exp((1500.3 + 23.5 * tOutside) / (234 + tOutside)));
+
+        [Browsable(false)]
+        public float pD2 => (float)(Math.Exp((1500.3 + 23.5 * tOut) / (234 + tOut)));
+
+
     }
 }
