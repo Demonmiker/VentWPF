a9644ec5a3bcae4b6d4201b058841aa1276a3002
Алексей Гуляев <stig153@outlook.com>
2021-09-16 22:35:45 +0300

    Шапка отчёта готова

--- VentWPF/DocX/DocX_Main.cs
+++ VentWPF/DocX/DocX_Main.cs
@@ -22,42 +22,52 @@ namespace VentWPF.DocX
             
             //Колонтитул 
             foreach (Section section in document.Sections)
-            {
-                /*
-                Word.Range headerRange = section.Headers[WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
-                headerRange.Fields.Add(headerRange, WdFieldType.wdFieldPage);
-                headerRange.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
-                headerRange.Font.ColorIndex = WdColorIndex.wdBlue;
-                headerRange.Font.Size = 10;
-                headerRange.Text = "VegaAir";*/
+            {               
                 
-                Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
-                document.Frames.Add(headerRange);
-                //Place Frame at top of page
-                headerRange.Frames[1].RelativeVerticalPosition = Word.WdRelativeVerticalPosition.wdRelativeVerticalPositionPage;
-                headerRange.Frames[1].VerticalPosition = 0;
-                headerRange.Frames[1].Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleNone;
-                //Add image
-                object docRange = headerRange.Frames[1].Range;
-                string imagePath = @"C:\Users\stig1\Desktop\wood-3.jpg";
-                document.InlineShapes.AddPicture(imagePath, ref missing, ref missing, ref docRange);
+                Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;                
+                Table Table = document.Tables.Add(headerRange, 1, 3, ref missing, ref missing);
+                Table.Borders.Enable = 0; //для тестов
 
-                headerRange.Frames[2].Range.Text = "VegaAir";
-                headerRange.Frames[2].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphRight;
-                headerRange.Frames[2].Range.Font.Size = 10;
+                
+                
+
+                Table.Rows[1].Cells[2].Range.Text = "  170025, Тверская область, г. Тверь \n  ул. Бочкина, д.18, строение 3 офис 3 \n  Тел.: +7 (4822) 74-42-87 \n  ivv@vega-air.com";
+                Table.Rows[1].Cells[2].Range.Font.Bold = 1;
+                Table.Rows[1].Cells[2].Range.Font.Size = 10;
+                Table.Rows[1].Cells[2].Range.Font.Name = "verdana";
+                Table.Rows[1].Cells[1].Range.Frames.Add(headerRange);
+                //настройки отображения
+                Table.Rows[1].Cells[3].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                Table.Rows[1].Cells[1].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;                
+                Table.Rows[1].Cells[2].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                
+                //Table.Rows[1].Cells[1].Width = 200;
+                //Table.Rows[1].Cells[2].Width = 180;
+                
+                //Table.Rows[1].Cells[3].Width = 100;
+                //------                              
+                //Add image
+                object docRange = Table.Rows[1].Cells[1].Range;
+                string imagePath = @"C:\Users\stig1\Desktop\logo.png";
+                document.InlineShapes.AddPicture(imagePath, LinkToFile: true, SaveWithDocument: true, Range: docRange);
+                docRange = Table.Rows[1].Cells[3].Range;
+                imagePath = @"C:\Users\stig1\Desktop\qr-code.gif";
+                document.InlineShapes.AddPicture(imagePath, LinkToFile: true, SaveWithDocument: true, Range: docRange);
+
             }
 
 
             document.Content.SetRange(0, 0);
             document.Content.Text = "This is test document " + Environment.NewLine;
-
-            //Заголовок 
+            
+            //Заголовок
+            /*
             Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
             object styleHeading1 = "Heading 1";
             para1.Range.set_Style(ref styleHeading1);
             para1.Range.Text = "Para 1 text";
             para1.Range.InsertParagraphAfter();
-            
+            Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
 
             //таблица 5х5 
             Table firstTable = document.Tables.Add(para1.Range, 5, 5, ref missing, ref missing);
@@ -85,13 +95,13 @@ namespace VentWPF.DocX
                     }
                 }
             }
-
+            */
             //Сохранение            
-            document.Save();
+            document.Save();/*
             document.Close(ref missing, ref missing, ref missing);
             document = null;
             winword.Quit(ref missing, ref missing, ref missing);
-            winword = null;
+            winword = null;*/
 
         }
     }
