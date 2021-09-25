2c70f3bb74439061cb0284ab32b4d194158016c8
Алексей Гуляев <stig153@outlook.com>
2021-09-26 00:32:34 +0300

    DocX_Main: Разбито на функции, базовая модель взаимодействия (тесты проведены, работает корректно, нуждается в заполнении и дитейлинге)

--- VentWPF/DocX/DocX_Main.cs
+++ VentWPF/DocX/DocX_Main.cs
@@ -7,29 +7,28 @@ namespace VentWPF.DocX
 {
     class DocX_Main
     {
+        object missing = System.Reflection.Missing.Value;
+        Application winword = new Application();
+
+        #region[testStrArea]
+        string HdrText = "Заказ №25564-2";
+        #endregion
         public void test()
         {
             CreateDocument();
         }
-        public void CreateDocument()
+
+        public void HeaderINIT(Document document)
         {
-             
-            Application winword = new Application();             
-            winword.ShowAnimation = false;              
-            winword.Visible = false;             
-            object missing = System.Reflection.Missing.Value;            
-            Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
-            
-            //Колонтитул 
             foreach (Section section in document.Sections)
-            {               
-                
-                Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;                
+            {
+
+                Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
                 Table Table = document.Tables.Add(headerRange, 1, 3, ref missing, ref missing);
                 Table.Borders.Enable = 0; //для тестов
 
-                
-                
+
+
 
                 Table.Rows[1].Cells[2].Range.Text = "  170025, Тверская область, г. Тверь \n  ул. Бочкина, д.18, строение 3 офис 3 \n  Тел.: +7 (4822) 74-42-87 \n  ivv@vega-air.com";
                 Table.Rows[1].Cells[2].Range.Font.Bold = 1;
@@ -38,12 +37,12 @@ namespace VentWPF.DocX
                 Table.Rows[1].Cells[1].Range.Frames.Add(headerRange);
                 //настройки отображения
                 Table.Rows[1].Cells[3].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
-                Table.Rows[1].Cells[1].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;                
+                Table.Rows[1].Cells[1].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
                 Table.Rows[1].Cells[2].Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
-                
+
                 //Table.Rows[1].Cells[1].Width = 200;
                 //Table.Rows[1].Cells[2].Width = 180;
-                
+
                 //Table.Rows[1].Cells[3].Width = 100;
                 //------                              
                 //Add image
@@ -55,21 +54,17 @@ namespace VentWPF.DocX
                 document.InlineShapes.AddPicture(imagePath, LinkToFile: true, SaveWithDocument: true, Range: docRange);
 
             }
-
-
-            document.Content.SetRange(0, 0);
-            document.Content.Text = "This is test document " + Environment.NewLine;
-            
-            //Заголовок
-            /*
+        }
+        public void OrderStatistics(Document document, string HText)
+        {
             Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
-            object styleHeading1 = "Heading 1";
-            para1.Range.set_Style(ref styleHeading1);
-            para1.Range.Text = "Para 1 text";
+            //object styleHeading1 = "Heading 1";
+            //para1.Range.set_Style(ref styleHeading1);
+            para1.Range.Text = HText;
             para1.Range.InsertParagraphAfter();
-            Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
-
-            //таблица 5х5 
+        }
+        public void tableINIT(Document document, Paragraph para1)
+        {
             Table firstTable = document.Tables.Add(para1.Range, 5, 5, ref missing, ref missing);
 
             firstTable.Borders.Enable = 1;
@@ -77,25 +72,46 @@ namespace VentWPF.DocX
             {
                 foreach (Cell cell in row.Cells)
                 {
-                     
+
                     if (cell.RowIndex == 1)
                     {
                         cell.Range.Text = "Column " + cell.ColumnIndex.ToString();
-                        cell.Range.Font.Bold = 1;                        
+                        cell.Range.Font.Bold = 1;
                         cell.Range.Font.Name = "verdana";
-                        cell.Range.Font.Size = 10;                                                      
-                        cell.Shading.BackgroundPatternColor = WdColor.wdColorGray25;                         
+                        cell.Range.Font.Size = 10;
+                        cell.Shading.BackgroundPatternColor = WdColor.wdColorGray25;
                         cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
                         cell.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
 
-                    }                    
+                    }
                     else
                     {
                         cell.Range.Text = (cell.RowIndex - 2 + cell.ColumnIndex).ToString();
                     }
                 }
             }
+        }
+        public void CreateDocument()
+        {
+            winword.ShowAnimation = false;
+            winword.Visible = false;
+
+            Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+            HeaderINIT(document);
+            OrderStatistics(document, HdrText);
+            Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
+            tableINIT(document, para1);
+            /*
+            document.Content.SetRange(0, 0);
+            document.Content.Text = HText + Environment.NewLine;
             */
+
+
+            
+
+            //таблица 5х5 
+
             //Сохранение            
             document.Save();/*
             document.Close(ref missing, ref missing, ref missing);
