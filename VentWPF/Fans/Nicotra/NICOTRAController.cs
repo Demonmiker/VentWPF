690eab5c449f8a0a87e33fab121f16380d8b1df2
Алексей Гуляев <stig153@outlook.com>
2021-10-16 01:55:27 +0300

    Примерный стиль К3Г (нужны исправления)

--- VentWPF/Fans/Nicotra/NICOTRAController.cs
+++ VentWPF/Fans/Nicotra/NICOTRAController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 
 namespace VentWPF.Fans.Nicotra
 {
-    class NICOTRAController
+    class NICOTRAController : IController<NICOTRARequest, List<NICOTRAFanData>>
     {
         [DllImport(@"C:\Users\stig1\source\repos\3type\3type\bin\x86\Nicotra.dll", CallingConvention = CallingConvention.StdCall)]
         public static extern int GET_CALCULATION_FANALONE(short s1, short s2, double[] IN, string KEY, short z1, short z2, double[] OUT);
@@ -15,17 +17,35 @@ namespace VentWPF.Fans.Nicotra
 
 
 
-        void test()
+        public List<NICOTRAFanData> GetResponce(NICOTRARequest request)
+        {            
+            var ids = GetIDs();
+            List<NICOTRAFanData> res = new();
+            foreach (var id in ids)
+            {
+                var list = GetFanInfo(id, request);
+                NICOTRAFanData data = new NICOTRAFanData(id, list);
+                res.Add(data);
+            }
+            return res;
+        }
+
+        public IEnumerable<string> GetIDs()
         {
+            var bufferIDs = new string(new Char(), 30000);
+            int n = GET_PRODUCTS(ref bufferIDs);
+            var str = bufferIDs.ToString();
+            return str.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Where(x => !x.StartsWith("K3G")); //исправить
+        }
+
+        public IEnumerable<string> GetFanInfo(string id, NICOTRARequest req)
+        {
+            req.ID = id;
+            var bufferInfo = new string(new Char(), 4000);
+            var r = req.GetRequest();
+
             var KEY = new string(new Char(), 15);
             KEY = "AT 7-7 S";
-            
-            
-            var LIST = new string(new Char(), 30000);
-            int n = GET_PRODUCTS(ref LIST);
-            var str = LIST.ToString();
-            
-
             double[] IN =
             {
                 1, 1, 0, 24, 5, 600, 100, 150, 0, 0, 0, 0, 0
@@ -38,7 +58,18 @@ namespace VentWPF.Fans.Nicotra
             foreach (int i in OUT)
             {
                 OUT[i] = 0;
-            } 
+            }
+
+            GET_CALCULATION_FANALONE(s1, s2, IN, KEY, z1, z2, OUT);
+            return bufferInfo.Split(new[] { ';' });
+        }
+
+        void test()
+        {
+            
+                        
+
+             
             int e = GET_CALCULATION_FANALONE(s1, s2, IN, KEY, z1, z2, OUT);
         }
 
---
+++ VentWPF/Fans/Nicotra/NICOTRARequest.cs
@@ -0,0 +1,26 @@
+﻿
+namespace VentWPF.Fans.Nicotra
+{
+    internal class NICOTRARequest : IRequest<string>
+    {
+        public string ID { get; set; }
+
+        public float Volumenstrom { get; set; } = 0;
+                
+
+        public float AirDens { get; set; }
+
+        public float Altitude { get; set; }
+
+        public float AirTemperature { get; set; }
+
+        public float RequiredPressure { get; set; } = 0;
+
+        public int V { get; set; } = 0;
+
+        public string GetRequest()
+        {
+            return $"{ID};{AirDens};{Altitude};{AirTemperature};{RequiredPressure};{Volumenstrom};{V};";
+        }
+    }
+}
