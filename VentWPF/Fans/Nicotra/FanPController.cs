335b73b47f57937e718d1fbc0a79d65fc1549f7d
Алексей Гуляев <stig153@outlook.com>
2021-10-18 16:49:09 +0300

    Nicotra

--- VentWPF/Fans/Nicotra/FanPController.cs
+++ VentWPF/Fans/Nicotra/FanPController.cs
@@ -1,12 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
 
 namespace VentWPF.Fans.Nicotra
 {
-    class FanPController
+    class FanPController : IController<FanPRequest, List<FanPData>>
     {
+        //Получение расчёта по одному вентилятору
+        [DllImport(@"C:\Users\stig1\source\repos\3type\3type\bin\x86\Nicotra.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int GET_CALCULATION_FANALONE(short s1, short s2, double[] IN, string KEY, short z1, short z2, double[] OUT);
+
+        //получения списка вентиляторов
+        [DllImport(@"C:\Users\stig1\source\repos\3type\3type\bin\x86\Nicotra.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+        public static extern int GET_PRODUCTS([MarshalAsAttribute(UnmanagedType.VBByRefStr)] ref string LIST);
+
+
+
+        public List<FanPData> GetResponce(FanPRequest request)
+        {
+            var KEY = GetKEYs();
+            List<FanPData> res = new();
+            foreach (var id in KEY)
+            {
+                var list = GetFanInfo(id, request);
+                FanPData data = new NICOTRAFanData(id, list);
+                res.Add(data);
+            }
+            return res;
+        }
+
+        public IEnumerable<string> GetKEYs()
+        {
+            var bufferIDs = new string(new Char(), 30000);
+            int n = GET_PRODUCTS(ref bufferIDs);
+            var str = bufferIDs.ToString();
+            return str.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Where(x => !x.StartsWith("K3G")); //исправить
+        }
+
+        public IEnumerable<string> GetFanInfo(string id, FanPRequest req)
+        {
+            var bufferInfo = new string(new Char(), 4000);
+            var r = req.GetRequest();
+
+            //временные данные----------
+            var KEY = new string(new Char(), 15);
+            KEY = "AT 7-7 S";
+            double[] IN =
+            {
+                1, 1, 0, 24, 5, 600, 100, 150, 0, 0, 0, 0, 0
+            };
+            short s1 = 0;
+            short s2 = 0;
+            short z1 = 0;
+            short z2 = 0;
+            double[] OUT = new double[31];
+            foreach (int i in OUT)
+            {
+                OUT[i] = 0;
+            }
+            //временные данные конец-------
+
+            GET_CALCULATION_FANALONE(s1, s2, IN, KEY, z1, z2, OUT);
+            return bufferInfo.Split(new[] { ';' });
+        }
     }
 }
+
+/*
+ Справка по Nicotra данным:
+GET_CALCULATION_FANALONE:
+s1, s2, z1, z2 -не используются, значения могут быть любыми - на вычисления не влияют. Желательно оставлять 0
+IN - массив double из входных параметров
+KEY - string имя-ключ вентилятора (подобие ID в K3G)
+OUT - выходные параметры массив double, после запроса массив OUT меняется
+P.S. все имена даны согласно документации
+
+GET_PRODUCTS возвращает строку вида:
+
+NICOTRA\r\n
+AT 7-7 S,600303,Fan with forward-curved wheel\r\n
+AT 9-7 S,600306,Fan with forward-curved wheel\r\n
+AT 9-9 S,600308,Fan with forward-curved wheel\r\n
+AT 10-8 S,600312,Fan with forward-curved wheel\r\n
+AT 10-10 S,600314,Fan with forward-curved wheel\r\n
+AT 12-9 S,600318,Fan with forward-curved wheel\r\n
+...
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Нужны будут не все вентидляторы, только RDH, ADH, возможно ещё какие-то, не отвечают какие именно. Надо придумать как учитывать этот момент
+и не просчитывать ненужные
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+Расшифровка:
+AT 7-7 S - имя, оно же KEY
+600303 - кодовый номер (назначение неизвестно, предположительно - номер в каталоге)
+Fan with forward-curved wheel - краткое описание
+
+ */
---
+++ VentWPF/Fans/Nicotra/FanPRequest.cs
@@ -0,0 +1,58 @@
+﻿using valid = VentWPF.Tools;
+using PropertyTools.DataAnnotations;
+
+namespace VentWPF.Fans.Nicotra
+{
+    internal class FanPRequest : IRequest<string>
+    {
+        public string KEY { get; set; }
+
+        public double Option { get; set; } = 1; //1 - Air volume and static pressure 2 - Air volume and total pressure
+
+        public double InstType { get; set; } = 1; // 1 - free inlet free outlet  2 - free inlet ducted outlet 
+
+        public double AirDensity { get; set; } = 0; //0 - расчёт от высоты и температуры
+
+        public double AirTemperature { get; set; } = 24; //температура
+
+        public double Height { get; set; } = 1; //высота в метрах
+
+        public double FlowRate { get; set; } = 600; //VFlow
+
+        public double StaticPressure { get; set; } = 100; //Статическое сопр давления
+
+        public double TotalPressure { get; set; } = 150; //Тотальное(?)
+
+        public double Speed { get; set; } = 0; //оставить 0
+
+        public double ShaftPower { get; set; } = 0; //оставить 0
+
+        public double Efficiency { get; set; } = 0;//оставить 0
+
+        public double SoundPowerLevel { get; set; } = 0;//оставить 0
+
+        [valid.Range(1, 2)]
+        public double PowerCorrection { get; set; } = 0;  //оставить 0     
+
+    }
+}
+/* порядок в массиве IN[]:
+
+Option
+InstType
+AirDensity
+AirTemperature
+Height
+FlowRate
+StaticPressure
+TotalPressure
+Speed
+ShaftPower
+Efficiency
+SoundPowerLevel
+PowerCorrection
+
+
+
+
+*/
