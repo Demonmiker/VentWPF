6e571b340c08e404c8db777deef0e6d547482f39
Алексей Гуляев <stig153@outlook.com>
2021-10-18 16:52:48 +0300

    Fans (Nicotra + K3G)

--- VentWPF/ViewModel/Strings.cs
+++ VentWPF/ViewModel/Strings.cs
@@ -22,6 +22,8 @@
 
         public const string fmm = "{0} мм";
 
+        public const string fm = "{0} М";
+
         public const string fT = "{0:0.0}°C";
 
         public const string fkW = "{0:0.00} кВт";
@@ -34,11 +36,25 @@
 
         public const string fm3 = "{0:0.00} м³";
 
+        public const string fkgm3 = "{0:0.00} кг/м³";
+
+        public const string fm3Ps = "{0:0.00} м³/сек";
+
+        public const string fm3Ph = "{0:0.00} м³/час";
+
+        public const string fdB = "{0:0.00} дБ";
+
+        public const string fRotate = "{0:0.00} 1/Мин";
+
+        public const string fV = "{0:0.00}В";
+
         public const string MasFr = "{0:0.00} Кг/Час";
 
         public const string fDate = "{0:dd/MM/yyyy}";
 
         public const string fNull = "{0:0.00} [X]";
+
+        public const string fEmpty = "{0:0.00} ";
 
         public const string errorRange = "Значение должно быть между {1} и {2}";
 
